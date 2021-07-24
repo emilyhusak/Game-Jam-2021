8ee48c2d78c2d3460bfe5546d2960d389ba5d5ac
sadiecrawford <sadiecrawford@ufl.edu>
2021-07-24 13:15:41 -0400

    Nighttime sky change

---
+++ Game Jam 2021/Assets/NighttimeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NighttimeController : MonoBehaviour
+{
+    public Material nightMat;
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("triggered");
+        RenderSettings.skybox = nightMat;
+    }
+}
