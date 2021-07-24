7023428c58c1016e32c32fb4e5eb4b9ebf8a7f9d
sadiecrawford <sadiecrawford@ufl.edu>
2021-07-23 23:41:01 -0400

    Followers

---
+++ Game Jam 2021/Assets/Follow.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+    public GameObject toFollow, follower;
+    public Vector3 dist;
+
+    // Update is called once per frame
+    void Update()
+    {
+        follower.transform.position = toFollow.transform.position - dist;
+    }
+}
